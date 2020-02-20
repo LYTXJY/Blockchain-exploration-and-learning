b151603c102936bbaaa7af5fe2d4b11f2e65d7fe
xjy <18822148155@163.com>
2020-02-20 09:33:09 +0800

    19

--- Code/src/go_code/chapter2/project01/main/hello.go
+++ Code/src/go_code/chapter2/project01/main/hello.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main(){
 	fmt.Println("hello world through golang")
+	fmt.Println("hello world through golang")//pycharm用的多，它的复制语句是ctrl+D
+	fmt.Println("hello world through golang")//而VS CODE中比较麻烦
+	fmt.Println("hello world through golang")//是 alt+shift+下箭头
 }
 
